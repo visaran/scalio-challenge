2212cfe2bafa8208074ee40b11eca0dac5872bd2
Vinicius Saran <vi_saran@hotmail.com>
2021-11-16 15:38:33 -0300

    feat: basic search component fetching data

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,15 +1,38 @@
-import React, { FunctionComponent }  from 'react'
+import axios from "axios";
+import React, { FunctionComponent, useState } from "react";
 
-interface SearchProps {
-  
-}
- 
+interface SearchProps {}
+
 const Search: FunctionComponent<SearchProps> = () => {
+  const [login, setLogin] = useState("");
+  const [data, setData] = useState([]);
+
+  async function onSubmitLogin(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (!login) return;
+
+    async function fetchData() {
+      const { data } = await axios(
+        `https://api.github.com/search/users?q=${login} in:login`
+      );
+      setData(data);
+    }
+    fetchData();
+  }
+
   return (
     <div>
-      HELLO SEARCH
+      <form onSubmit={onSubmitLogin}>
+        <input
+          type="text"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <div>{login}</div>
     </div>
   );
-}
- 
+};
+
 export default Search;
