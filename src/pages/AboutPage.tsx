d5216557e06724bee56c8d0baa4343d869a13dae
Vinicius Saran <vi_saran@hotmail.com>
2021-11-16 17:03:54 -0300

    feat: about page

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+interface AboutPageProps {}
+
+const AboutPage: FunctionComponent<AboutPageProps> = () => {
+  return <div>oi</div>;
+};
+
+export default AboutPage;
