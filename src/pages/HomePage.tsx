298f59b7dcaeda30322eb10fe7a1739756039adc
Vinicius Saran <vi_saran@hotmail.com>
2021-11-17 08:56:39 -0300

    feat: home context api

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,36 @@
-import { FunctionComponent } from "react";
+import { createContext, FunctionComponent, useCallback, useState } from "react";
 import Results from "../components/Results";
 import Search from "../components/Search";
 
-interface HomePageProps {}
+interface IUser {
+  avatar_url: number;
+  login: string;
+  html_url: string;
+  type: string;
+}
 
-const HomePage: FunctionComponent<HomePageProps> = () => {
+interface UsersContextData {
+  users: IUser[];
+  setUsers: (users: IUser[]) => void;
+}
+
+export const usersContextDefaultValue: UsersContextData = {
+  users: [],
+  setUsers: () => {},
+};
+
+export const UsersContext = createContext<UsersContextData>(
+  usersContextDefaultValue
+);
+
+export const HomePage: FunctionComponent = () => {
+  const [users, setUsers] = useState<IUser[]>(usersContextDefaultValue.users);
+
   return (
-    <div>
+    <UsersContext.Provider value={{ users, setUsers }}>
       <Search />
       <Results />
-    </div>
+    </UsersContext.Provider>
   );
 };
 
