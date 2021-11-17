762c42f3d2b04b420be7346fe278cba3ec4e7a65
Vinicius Saran <vi_saran@hotmail.com>
2021-11-17 08:56:53 -0300

    feat: basic results page

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,9 +1,43 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useContext, useState } from "react";
+import { UsersContext } from "../pages/HomePage";
+import { Table } from "antd";
 
 interface ResultsProps {}
 
 const Results: FunctionComponent<ResultsProps> = () => {
-  return <div>RESULTS</div>;
+  const [currentPage, setCurrentPage] = useState<number>(0);
+
+  const { users } = useContext(UsersContext);
+
+  console.log("users", users);
+
+  function onChange() {
+    setCurrentPage(currentPage + 1);
+    console.log("currentPage", currentPage);
+  }
+
+  const columns = [
+    { title: "Avatar URL", key: "id", dataIndex: "avatar_url" },
+    { title: "Login", key: "id", dataIndex: "login" },
+    { title: "Type", key: "id", dataIndex: "type" },
+  ];
+  return (
+    <div>
+      {users && users.length ? (
+        <Table
+          columns={columns}
+          pagination={{
+            pageSize: 9,
+            total: 100,
+            onChange: () => onChange(),
+            current: currentPage,
+          }}
+          loading={{ spinning: !Boolean(users.length) }}
+          dataSource={users}
+        />
+      ) : null}
+    </div>
+  );
 };
 
 export default Results;
