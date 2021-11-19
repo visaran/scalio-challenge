899a9e28cd11d58f403046b0a1ec9abe22744bac
Vinicius Saran <vi_saran@hotmail.com>
2021-11-19 20:18:54 -0300

    enhancement: add types

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
 
 import searchReducer from "./components/Search/Search.slice";
 
@@ -9,5 +10,8 @@ const store = configureStore({
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
 
 export default store;
